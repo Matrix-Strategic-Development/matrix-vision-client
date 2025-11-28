ef4614a099d8978d86961015d1a299507da60c23
Vlad <osadchiy900@icloud.com>
2025-11-28 15:21:36 +0300

    Add Base Dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${mulish.variable} ${poppins.variable} font-sans antialiased`}
+        className={`${mulish.variable} ${poppins.variable} font-sans antialiased dark`}
       >
         {children}
       </body>
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -76,8 +76,21 @@ const CardFooter = React.forwardRef<
 ));
 CardFooter.displayName = "CardFooter";
 
+const CardAction = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("absolute right-4 top-4", className)}
+    {...props}
+  />
+));
+CardAction.displayName = "CardAction";
+
 export {
   Card,
+  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
